8c87985275cf65157bbaa42e345e2c79f9008e03
Reuben Frith <reubenfrith007@gmail.com>
2024-06-12 17:21:14 +1000

    remove files

--- cdk/lib/config.ts
+++
@@ -1,3 +0,0 @@
-export const websiteUrl = "XXX"
-export const accountId = "128384115514"
-export const regionId = "ap-southeast-2"
--- lambda/pdfLambda.ts
+++
@@ -1,5 +0,0 @@
-
-
-module.exports.nvrGetBasicReportData = async (event) => {
-  return event
-}
